1e6ee62049991659025081861a74e79fe57886f0
Martell-Francisco <franciscomartell@att.net>
2023-06-02 23:11:49 -0500

    added Image

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = 500;
+canvas.height = 700;
+const explosions = [];
+
+class Explosion {
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+        this.spriteWidth = 200;
+        this.spriteHeight = 179;
+        this.width = this.spriteWidth/2;
+        this.height = this.spriteHeight*.5;
+        this.image = new Image();
+        this.image.src = 'boom.png';
+        this.frame = 0; 
+    }
+    update(){
+        this.frame++;
+    }
+    draw(){
+        //ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
+        ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
+    }
+}
 
