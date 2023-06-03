eac4d9fe6172a06f785b6817534edd48915fabb7
Martell-Francisco <franciscomartell@att.net>
2023-06-02 23:50:56 -0500

    commit

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext('2d');
 canvas.width = 500;
 canvas.height = 700;
 const explosions = [];
+let canvasPosition = canvas.getBoundingClientRect();
 
 class Explosion {
     constructor(x, y){
@@ -10,18 +11,39 @@ class Explosion {
         this.y = y;
         this.spriteWidth = 200;
         this.spriteHeight = 179;
-        this.width = this.spriteWidth/2;
-        this.height = this.spriteHeight*.5;
+        this.width = this.spriteWidt*.7;
+        this.height = this.spriteHeight*.7;
         this.image = new Image();
-        this.image.src = 'boom.png';
+        this.image.src = 'img/boom.png';
         this.frame = 0; 
+        this.timer = 0;
+
     }
     update(){
-        this.frame++;
+        this.timer++;
+        if(this.timer % 10 ===0){
+             this.frame++;
+            }
     }
     draw(){
         //ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
-        ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
+        ctx.drawImage(this.image, this.spriteWidth*this.frame, 0, this.spriteWidth,
+            this.spriteHeight, this.x, this.y, this.width, this.height); 
     }
 }
 
+window.addEventListener('click', function(e){
+    let positionX = e.x - canvasPosition.left;
+    let positionY = e.y - canvasPosition.top;
+    explosions.push(new Explosion(positionX, positionY));
+});
+
+function animate(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i< explosions.length; i++){
+        explosions[i].update();
+        explosions[i].draw();
+    }
+    requestAnimationFrame(animate);
+}
+animate();
